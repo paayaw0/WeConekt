463918b015d847f6f60b00465c1b381f1e51c177
Paa Yaw <paayaw.dev@gmail.com>
2023-10-17 11:00:49 +0000

    online status determined only by streaming via ActionCable, remove callback to update online status

--- app/channels/online_status_channel.rb
+++ app/channels/online_status_channel.rb
@@ -1,12 +1,20 @@
 class OnlineStatusChannel < ApplicationCable::Channel
   def subscribed
     OnlineStatusService.call(current_user, connect_online: true)
-
-    stream_from 'online_users'
+    room_id = params[:room_id]
+    send_message({body: "#{current_user.name} just joined", room_id:})
+    
+    stream_from "online_users_room_id_#{room_id}"
   end
 
   def unsubscribed
     OnlineStatusService.call(current_user)
+    room_id = params[:room_id]
+    send_message({body: "#{current_user.name} just left!", room_id:})
     current_room_connection.update(current: false)
+  end
+
+  def send_message(data={})
+    ActionCable.server.broadcast("online_users_room_id_#{params[:room_id]}", data)
   end
 end
--- app/javascript/channels/online_status_channel.js
+++ app/javascript/channels/online_status_channel.js
@@ -1,19 +1,30 @@
 import consumer from "channels/consumer"
 
-const onlineUsers = document.getElementById('online_users');
+// const onlineUsers = document.getElementById('online_users');
 // we only want to stream when this element appears in the dom
-if(onlineUsers) {
-  consumer.subscriptions.create("OnlineStatusChannel", {
-    connected() {
-      // Called when the subscription is ready for use on the server
-    },
+const roomId = document.getElementById('online_user_room_id').getAttribute('roomId');
 
-    disconnected() {
-      // Called when the subscription has been terminated by the server
-    },
+consumer.subscriptions.create({channel: "OnlineStatusChannel", room_id: roomId }, {
+  connected() {
+    // Called when the subscription is ready for use on the server
+  },
 
-    received(data) {
-      // Called when there's incoming data on the websocket for this channel
+  disconnected() {
+    // Called when the subscription has been terminated by the server
+  },
+
+  received(data) {
+    // Called when there's incoming data on the websocket for this channel
+    let ele = document.getElementById(`online_status_${data['room_id']}`);
+
+    if(data['body'].match(/joined/)){
+      ele.classList.remove('btn-default');
+      ele.classList.add('btn-success');
+      ele.innerText = 'online';
+    }else if(data['body'].match(/left/)){
+      ele.classList.remove('btn-success');
+      ele.classList.add('btn-default');
+      ele.innerText = 'offline';
     }
+  }
 });
-}
