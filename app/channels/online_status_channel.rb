8da57d18f5fe420937b61585d2180e202bfd723a
Paa Yaw <paayaw.dev@gmail.com>
2023-10-18 02:47:06 +0000

    broadcast from OnlineChannel to consumer; consumer toggles online visibility

--- app/channels/online_status_channel.rb
+++ app/channels/online_status_channel.rb
@@ -1,20 +1,32 @@
 class OnlineStatusChannel < ApplicationCable::Channel
   def subscribed
-    OnlineStatusService.call(current_user, connect_online: true)
-    room_id = params[:room_id]
-    send_message({body: "#{current_user.name} just joined", room_id:})
+    room_id = params[:room_id] || current_room&.id || current_user.current_room&.id
+    return unless room_id
     
-    stream_from "online_users_room_id_#{room_id}"
+    OnlineStatusService.call(current_user, connect_online: true)
+    send_message({
+                   body: "#{current_user.name} just joined", 
+                   room_id:,
+                   current_user_id: current_user.id
+                  })
+
+    stream_from "online_users_rooms_id_#{room_id}"
   end
 
   def unsubscribed
+    room_id = params[:room_id] || current_room&.id || current_user.current_room&.id
+    return unless room_id
+
     OnlineStatusService.call(current_user)
-    room_id = params[:room_id]
-    send_message({body: "#{current_user.name} just left!", room_id:})
-    current_room_connection.update(current: false)
+    send_message({
+                  body: "#{current_user.name} just left!", 
+                  room_id:,
+                  current_user_id: current_user.id
+                })
+    current_room_connection&.update(current: false)
   end
 
   def send_message(data={})
-    ActionCable.server.broadcast("online_users_room_id_#{params[:room_id]}", data)
+    ActionCable.server.broadcast("online_users_rooms_id_#{data[:room_id]}", data)
   end
 end
--- app/javascript/channels/online_status_channel.js
+++ app/javascript/channels/online_status_channel.js
@@ -2,29 +2,36 @@ import consumer from "channels/consumer"
 
 // const onlineUsers = document.getElementById('online_users');
 // we only want to stream when this element appears in the dom
-const roomId = document.getElementById('online_user_room_id').getAttribute('roomId');
+const onlineUsersRoom = document.getElementById('online_user_room_id');
+const roomId = onlineUsersRoom?.getAttribute('roomId');
+const userId = onlineUsersRoom?.getAttribute('userId');
 
-consumer.subscriptions.create({channel: "OnlineStatusChannel", room_id: roomId }, {
-  connected() {
-    // Called when the subscription is ready for use on the server
-  },
+if(onlineUsersRoom) {
+  consumer.subscriptions.create({channel: "OnlineStatusChannel", room_id: roomId }, {
+    connected() {
+      // Called when the subscription is ready for use on the server
+    },
 
-  disconnected() {
-    // Called when the subscription has been terminated by the server
-  },
+    disconnected() {
+      // Called when the subscription has been terminated by the server
+    },
 
-  received(data) {
-    // Called when there's incoming data on the websocket for this channel
-    let ele = document.getElementById(`online_status_${data['room_id']}`);
-
-    if(data['body'].match(/joined/)){
-      ele.classList.remove('btn-default');
-      ele.classList.add('btn-success');
-      ele.innerText = 'online';
-    }else if(data['body'].match(/left/)){
-      ele.classList.remove('btn-success');
-      ele.classList.add('btn-default');
-      ele.innerText = 'offline';
+    received(data) {
+      // Called when there's incoming data on the websocket for this channel
+      let ele = document.getElementById(`online_status_${data['room_id']}_${data['current_user_id']}`);
+      if(userId!==data['current_user_id']) {
+          if(ele) {
+            if(data['body'].match(/joined/)){
+              ele.classList.remove('btn-default');
+              ele.classList.add('btn-success');
+              ele.innerText = 'online';
+            }else if(data['body'].match(/left/)){
+              ele.classList.remove('btn-success');
+              ele.classList.add('btn-default');
+              ele.innerText = 'offline';
+            }
+          }
+      }
     }
-  }
-});
+  });
+}
