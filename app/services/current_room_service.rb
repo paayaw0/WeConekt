19500d3e1858256cc98e219c84ff3c495659ee6f
Paa Yaw <paayaw.dev@gmail.com>
2023-10-20 23:19:49 +0000

    resolve race condition by using pessimistic locking and rescuing when SQlite3::BusyException is raised

--- app/services/current_room_service.rb
+++ app/services/current_room_service.rb
@@ -4,16 +4,21 @@ class CurrentRoomService < ApplicationService
   def initialize(user, room, set_current_connection: true)
     @user = user
     @room = room
-    @connection = Connection.find_by(room_id: room.id, user_id: user.id)
+    @connection = user.connections.find_by(room_id: room.id)
     @set_current_connection = set_current_connection
   end
 
   def call
-    if set_current_connection
-      user.connections.update_all(current: false) # there can only be one current connection to reflect that there can only be on current_room at a time for the current user
-      connection.update(current: true, last_connected_at: DateTime.now)
-    else
-      connection.update(current: false)
+    max_tries = 5
+
+    user.connections.update_all(current: false) if set_current_connection # there can only be one current connection to reflect that there can only be on current_room at a time for the current user
+    connection.reload.with_lock do
+      connection.update(current: set_current_connection, last_connected_at: DateTime.now)
+    end
+  rescue ActiveRecord::StatementInvalid
+    if max_tries >= 0
+      max_tries -= 1
+      retry
     end
   end
 end
