055c697d6e1f5c01a6e55707dcea4d8777575027
Paa Yaw <paayaw.dev@gmail.com>
2023-11-29 19:47:44 +0000

    delete messages older than specified duration

--- app/sidekiq/disappearing_messaages_job.rb
+++ app/sidekiq/disappearing_messaages_job.rb
@@ -11,8 +11,9 @@ class DisappearingMessaagesJob
       room = room_config.room
 
       messages = room.messages.where(updated_at: room_config.updated_at...) # get all room messages post room config set date
-      messages_marked_for_deletion = messages.where(updated_at: duration.ago...) # get messages older than duration
+      messages_marked_for_deletion = messages.where(updated_at: ...duration.ago) # get messages older than duration
       shared_messages = SharedMessage.where(message_id: messages_marked_for_deletion.pluck(:id))
+
       shared_messages.destroy_all
       messages_marked_for_deletion.destroy_all
     end
